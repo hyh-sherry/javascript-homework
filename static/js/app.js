03b06fea7f724b6108b2f98018b52d8b30848bb6
hyh-sherry <49596048+hyh-sherry@users.noreply.github.com>
2019-09-07 21:23:46 -0400

    Add files via upload

---
+++ static/js/app.js
@@ -0,0 +1,35 @@
+// from data.js
+var tableData = data;
+
+// Write code that appends a table to your web page and then adds new rows of data for each UFO sighting.
+var tbody = d3.select("#ufo-table>tbody");
+
+tableData.forEach((observation) => {
+    var row = tbody.append("tr");
+    Object.entries(observation).forEach(([key,value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+    })
+});
+
+// Write JavaScript code that will listen for events and search through o find rows that match user input.
+var filterButton = d3.select("#filter-btn");
+
+filterButton.on("click", function() {
+    var inputDate = d3.select("#datetime").property("value");
+    var filteredObs = tableData.filter(observation => observation.datetime === inputDate);
+
+    // check the filtered observations
+    console.log(filteredObs);
+
+    // rewrite the table content
+    tbody.html("");
+
+    filteredObs.forEach((observation) => {
+        var row = tbody.append("tr");
+        Object.entries(observation).forEach(([key, value]) => {
+            var cell = row.append("td");
+            cell.text(value);
+        })
+    })
+});
