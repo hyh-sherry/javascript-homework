0ebe5aa2d694b861d981cf0586d939ee6215e15a
Yanhuan Huang <hyhuan0318@gmail.com>
2020-04-12 16:19:38 -0400

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -5,31 +5,33 @@ var tableData = data;
 var tbody = d3.select("#ufo-table>tbody");
 
 tableData.forEach((observation) => {
-    var row = tbody.append("tr");
-    Object.entries(observation).forEach(([key,value]) => {
-        var cell = row.append("td");
-        cell.text(value);
-    })
+  var row = tbody.append("tr");
+  Object.entries(observation).forEach(([key, value]) => {
+    var cell = row.append("td");
+    cell.text(value);
+  });
 });
 
-// Write JavaScript code that will listen for events and search through o find rows that match user input.
+// Write JavaScript code that will listen for events and search through obervations to find rows that match user input.
 var filterButton = d3.select("#filter-btn");
 
-filterButton.on("click", function() {
-    var inputDate = d3.select("#datetime").property("value");
-    var filteredObs = tableData.filter(observation => observation.datetime === inputDate);
+filterButton.on("click", function () {
+  var inputDate = d3.select("#datetime").property("value");
+  var filteredObs = tableData.filter(
+    (observation) => observation.datetime === inputDate
+  );
 
-    // check the filtered observations
-    console.log(filteredObs);
+  // check the filtered observations
+  console.log(filteredObs);
 
-    // rewrite the table content
-    tbody.html("");
+  // rewrite the table content
+  tbody.html("");
 
-    filteredObs.forEach((observation) => {
-        var row = tbody.append("tr");
-        Object.entries(observation).forEach(([key, value]) => {
-            var cell = row.append("td");
-            cell.text(value);
-        })
-    })
+  filteredObs.forEach((observation) => {
+    var row = tbody.append("tr");
+    Object.entries(observation).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
 });
